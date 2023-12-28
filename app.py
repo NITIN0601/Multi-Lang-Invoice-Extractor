7a0257d00097ba1abf9292fb5a509a31cc3b1e92
NITIN0601 <dmnitin97@gmail.com>
2023-12-27 21:54:20 -0500

    Version_2

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
-import os 
-
-load_dotenv() # Which loads all the env varibales from .env
-#GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-
+import os
 import streamlit as st
 import google.generativeai as genai
 from PIL import Image
+from io import BytesIO
+import fitz # PyMuPDF which uses PDF files
+
+load_dotenv() # Which loads all the env varibales from .env
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -24,6 +24,7 @@ def get_gemini_response(input, image, prompt):
     response = model.generate_content([input, image[0],prompt])
     return response.text
 
+
 def input_image_details(uploaded_file):
     """
     Function : to convert the image into bytes 
@@ -31,31 +32,52 @@ def input_image_details(uploaded_file):
     return: image bytes 
     """
     if uploaded_file is not None:
-        # Read the file into bytes
-        bytes_data = uploaded_file.getvalue()
-
-        image_parts = [
-            {
-                "mime_type": uploaded_file.type,
-                "data": bytes_data
-            }
-        ]
-        return image_parts
+        # Check if the uploaded file is a PDF
+        if uploaded_file.type == 'application/pdf':
+            # Convert PDF to image
+            images = pdf_to_images(uploaded_file)
+            return images
+        else:
+            # For other image types, read the file into bytes
+            bytes_data = uploaded_file.getvalue()
+            image_parts = [{"mime_type": uploaded_file.type, "data": bytes_data}]
+            return image_parts
     else:
         raise FileNotFoundError("No File Uploaded")
 
 
-# Streamlit app code working
 
+def pdf_to_images(uploaded_file):
+    pdf_images = []
+    pdf_document = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        pix = page.get_pixmap()
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        img_byte_array = BytesIO()
+        img.save(img_byte_array, format="PNG")
+        pdf_images.append({"image": img_byte_array.getvalue(), "page_num": page_num + 1})
+
+    pdf_document.close()
+    return pdf_images
+
+
+# Streamlit app code working
 st.set_page_config(page_title = " Multi-Lang Invoice Extractor ")
 st.header("Gemini GenAI Application : Multi-Lang Invoice Extractor ")
 input = st.text_input("Input Prompt: ",key = "input")
-uploaded_file = st.file_uploader("Choose an image of the invoice .....", type=["jpg","jpeg","png"])
-image = ""
+uploaded_file = st.file_uploader("Choose an image of the invoice .....", type=["jpg","jpeg","png","pdf"])
+images = []
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    if uploaded_file.type == 'application/pdf':
+        images = pdf_to_images(uploaded_file)
+    else:
+        images = [{"image": Image.open(uploaded_file).convert("RGB").save(None, "PNG"), "page_num": 1}]
+   
+for image_data in images:
+    st.image(Image.open(BytesIO(image_data["image"])), caption=f"Page {image_data['page_num']}", use_column_width=True)
 
 submit = st.button(" Tell me about the invoice ")
 
