5cca896378f798172b2e81907106d16dae78c26e
Nitin <dmnitin97@gmail.com>
2024-01-03 10:29:09 -0500

    Version_3.2

--- app.py
+++ app.py
@@ -31,20 +31,36 @@ def input_image_details(uploaded_file):
     Function : to convert the image into bytes 
     uploaded_file: Image input 
     return: image bytes 
+
     """
+
     if uploaded_file is not None:
-        # Check if the uploaded file is a PDF
-        if uploaded_file.type == 'application/pdf':
-            # Convert PDF to image
-            images = pdf_to_images(uploaded_file)
-            return images
-        else:
-            # Read the Imagefile into bytes
-            bytes_data = uploaded_file.getvalue()
-            image_parts = [{"mime_type": uploaded_file.type, "data": bytes_data}]
-            return image_parts
+        bytes_data = uploaded_file.getvalue()
+        image_parts = [{"mime_type": uploaded_file.type, "data": bytes_data}]
+        return image_parts
+       
     else:
-        raise FileNotFoundError("No File Uploaded")
+        raise FileNotFoundError("No File Uploaded")   
+
+
+    
+    # # Check if the uploaded file is a PDF
+    # if uploaded_file.type == 'application/pdf':
+    #     # Convert PDF to image
+    #     doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    #     image_parts = [] 
+    #     for i, page in enumerate(doc):
+    #         pix = page.get_pixmap()
+    #         bytes_data = pix.tobytes(output='png')
+    #         image_parts.append({"mime_type": "image/png",
+    #                             "data": bytes_data, 
+    #                             "page": i+1})
+    # else:
+    #     # Read the Imagefile into bytes
+    #     bytes_data = uploaded_file.getvalue()
+    #     image_parts = [{"mime_type": uploaded_file.type, "data": bytes_data}]
+    #     return image_parts
+
 
 
 # New function to handle PDF conversion 
@@ -73,18 +89,21 @@ def pdf_to_images(uploaded_file):
 st.set_page_config(page_title = " Multi-Lang Invoice Extractor ")
 st.header("Gemini GenAI Application : Multi-Lang Invoice Extractor ")
 input = st.text_input("Input Prompt: ",key = "input")
-file_type = st.radio ('Choose file type :',['Image','PDF'],index=0)
+file_type = st.radio ('Choose file type :',['Image'],index=0)
+# file_type = st.radio ('Choose file type :',['Image','PDF'],index=0) # TO-DO next version
 
 if file_type == 'Image':
     uploaded_file = st.file_uploader("Upload an Image ", type=["jpg","jpeg","png"])
-    image_data = Image.open(uploaded_file)
-    st.image(image_data, caption= "Uploaded Image", use_column_width=True)
+    if uploaded_file is not None:
+        image_data = Image.open(uploaded_file)
+        st.image(image_data, caption= "Uploaded Image", use_column_width=True)
 else:
     uploaded_file = st.file_uploader("Upload the PDF ", type=["pdf"])
-    image_data = pdf_to_images(uploaded_file)
-    for page in image_data:
-        image = Image.open(BytesIO(page["image"]))
-        st.image(image,caption=f"Page{page['page_num']}")
+    if uploaded_file is not None:
+        image_data = pdf_to_images(uploaded_file)
+        for page in image_data:
+            image = Image.open(BytesIO(page["image"]))
+            st.image(image,caption=f"Page{page['page_num']}")
 
 submit = st.button(" Tell me about the invoice ")
 
